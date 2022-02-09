95db4b06103912f6adc7ed2c9f617524f98eed8c
heather <htilley87@gmail.com>
2022-02-08 23:52:11 -0500

    added delete property functionality

--- src/main/java/com/claim/controller/PropertyController.java
+++ src/main/java/com/claim/controller/PropertyController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +31,11 @@ public class PropertyController {
 	@RequestMapping(value="/saveProperty", consumes=MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
 	public void submitNewPropertyDetails(@RequestBody Property property) {
 		propertyRepository.save(property);
+		
+	}
+	@RequestMapping(value="/deleteProperty/{id}",method = RequestMethod.DELETE )
+	public void deleteProperty(@PathVariable(value="id") Integer id){
+		propertyRepository.deleteById(id);
 		
 	}
 	/*
--- src/main/java/com/claim/entity/Property.java
+++ src/main/java/com/claim/entity/Property.java
@@ -19,7 +19,7 @@ public class Property {
 	@Column(name = "id")
 	private int id;
 	
-	@OneToOne(cascade = CascadeType.ALL)
+	@OneToOne(cascade = CascadeType.MERGE)
 	@JoinColumn(name="address_id")
 	private Address address;
 	
